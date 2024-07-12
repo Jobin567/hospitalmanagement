b1e0a857bbd6cc1870bc271387d41652cf9df09f
Jobin George <141149299+Jobin567@users.noreply.github.com>
2024-07-12 10:42:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 app.use(nocache ());
-app.use(session({secret:"Key",cookie:{maxAge:600000}}))
+app.use(session({secret:"Key",cookie:{maxAge:600000},saveUninitialized: true}))
 db.connect((err)=>{
   if(err) console.log("Connection Error"+err);
   else console.log("Database Connected to port 27017");
